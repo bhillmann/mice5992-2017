36ecce21c820d5609260a4ba0118852bcb988db7
Dan Knights <danknights@gmail.com>
2017-04-03 12:45:33 -0500

    subsetting multiple categories

--- tutorials/mct-v2/collapse_map.r
+++ tutorials/mct-v2/collapse_map.r
@@ -6,9 +6,16 @@
 
 args <- commandArgs(trail=T)
 m <- read.table(args[1], sep='\t', head=T,row=1,check=F,comment='')
-column <- args[2]
-if(!(column %in% colnames(m))) stop(paste("Column",column,"not in mapping file headers."))
-collapse.by <- m[,column]
+
+column <- strsplit(args[2],',')[[1]]
+
+if(!(all(column %in% colnames(m)))) stop(paste("Column",paste(column,collapse=','),"not in mapping file headers."))
+
+if(length(column) == 1){
+  collapse.by <- m[,column]
+} else if(length(column) >= 2){
+  collapse.by <- interaction(as.list(m[,column]))
+}
 
 m2 <- sapply(1:ncol(m),function(j) {splitx <- split(m[,j],collapse.by); if(class(m[,j]) == "numeric") sapply(splitx,function(xx) mean(xx[!is.na(xx)])) else sapply(splitx,function(xxx) names(sort(table(as.character(xxx)),decreasing=TRUE))[1])}); colnames(m2) <- colnames(m)
 
